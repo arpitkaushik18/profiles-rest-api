2372aa55b93b76b45e209e37ed36a94be45724ba
arpitkaushik18 <darpitkaushik@gmail.com>
2018-12-21 12:30:42 +0530

    enabled the django admin.

--- src/profile_project/profiles_api/models.py
+++ src/profile_project/profiles_api/models.py
@@ -23,6 +23,15 @@ class UserProfileManager(BaseUserManager):
 
         return user
 
+    def create_superuser(self,email,name,password):
+        """Creates and saves a new superuser with guven details."""
+
+        user = self.create_user(email,name,password)
+
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
 class UserProfile(AbstractBaseUser,PermissionsMixin):
     """ Represents a "user profile" inside our system."""
 
