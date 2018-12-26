a6a9a007b9c7035b63ac3611e3f3d9aa21103812
arpitkaushik18 <darpitkaushik@gmail.com>
2018-12-26 12:16:00 +0530

    Added search funtion to UserProfileViewset.

--- src/profile_project/profiles_api/views.py
+++ src/profile_project/profiles_api/views.py
@@ -3,12 +3,14 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.authentication import TokenAuthentication
 from rest_framework import status
+from rest_framework import filters
 
 from . import serializers
 from . import models
 from . import permissions
-from rest_framework.authentication import TokenAuthentication
+
 # Create your views here.
 
 class HelloApiView(APIView):
@@ -114,3 +116,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.object.all()
     authentication_class = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name','email',)
