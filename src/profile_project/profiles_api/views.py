05dad63beb86496a5f617192870be35a5c75547d
arpitkaushik18 <darpitkaushik@gmail.com>
2018-12-26 19:08:59 +0530

    Added Login API.

--- src/profile_project/profiles_api/views.py
+++ src/profile_project/profiles_api/views.py
@@ -6,6 +6,8 @@ from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import status
 from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.serializers import AuthTokenSerializer
 
 from . import serializers
 from . import models
@@ -118,3 +120,15 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name','email',)
+
+
+class LoginViewSet(viewsets.ViewSet):
+    """checks email and password and return an auth token."""
+
+    serializer_class = AuthTokenSerializer
+
+    def create(self, request):
+        """Use the ObtainAuthToken APIView to validate and create a token."""
+
+        return ObtainAuthToken().post(request)
+        
