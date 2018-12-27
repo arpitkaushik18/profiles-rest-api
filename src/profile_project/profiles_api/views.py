e7db4712bcb9f789bfbc929775c912dacc12504d
arpitkaushik18 <darpitkaushik@gmail.com>
2018-12-27 12:20:34 +0530

    Added improved permission to user profile API.

--- src/profile_project/profiles_api/views.py
+++ src/profile_project/profiles_api/views.py
@@ -8,7 +8,8 @@ from rest_framework import status
 from rest_framework import filters
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.serializers import AuthTokenSerializer
-
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 from . import serializers
 from . import models
 from . import permissions
@@ -131,4 +132,17 @@ class LoginViewSet(viewsets.ViewSet):
         """Use the ObtainAuthToken APIView to validate and create a token."""
 
         return ObtainAuthToken().post(request)
-        
+
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """Handels creating, reading and updating profile feed item."""
+
+    authentication_classes = (TokenAuthentication,)
+    serializer_class =serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (permissions.PostOwnStatus, IsAuthenticated)
+
+    def perform_create(self, serializer):
+        """Sets the user profile to the logged in user."""
+
+        serializer.save(user_profile=self.request.user)
